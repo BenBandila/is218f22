927624b1f15353918427b57ca2d5809a3cfad95b
BenBandila <benbandila11@gmail.com>
2022-02-03 10:44:47 -0500

    Changed the name on the homepage

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keith"
+        name = "Ben"
         return render_template('index.html', name=name)
