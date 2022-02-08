eec2bf6ecad219d95e438c95b29e3a0c6d4236a4
BenBandila <benbandila11@gmail.com>
2022-02-08 10:42:54 -0500

    Changed my Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Ben"
+        name = "Ben B"
         return render_template('index.html', name=name)
